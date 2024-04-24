518e61b35dff0222ad448fa25f5b977176c7d1cb
Younes M'RABTI <younesmrabti50@gmail.com>
2024-04-24 18:00:34 +0100

    Update Social Links

--- pages/account.js
+++ pages/account.js
@@ -1,19 +1,40 @@
 import Link from 'next/link'
-import Head from 'next/head';
+import Head from 'next/head'
+import Layout from '../components/layout'
+import Image from 'next/image'
+import styles from '../components/layout.module.css'
+import utilStyles from '../styles/utils.module.css'
 
-export default function Account() {
+export async function getServerSideProps() {
+    const data = await fetch('https://randomuser.me/api/');
+    const randomuser = await data.json();
+    return {
+        props: {
+            randomuser
+        }
+    }
+}
+export default function Account({ randomuser}) {
+    const user = randomuser.results[0];
     return (
-        <>
+        <Layout randomuser={user}>
             <Head>
-                <title>My Account</title>
+                <title>{user.email}</title>
             </Head>
-            <h1>Username</h1>
-            <h2>Email</h2>
-            <h2>
-                <Link href="/">
-                    <a>Back to home</a>
-                </Link>
-            </h2>
-        </>
+            <header className={styles.header}>
+                <Image
+                    priority
+                    src={user.picture.large}
+                    className={utilStyles.borderCircle}
+                    height={144}
+                    width={144}
+                    alt={Object.values(user.name).join(' ') }
+                />
+                <h1 className={utilStyles.heading2Xl}>{Object.values(user.name).join(' ')}</h1>
+            </header>
+            <h1>Username{' : ' + user.login.username}</h1>
+            <h2>Email{' : '+user.email}</h2>
+            <h2>Age{' : '+user.dob.age}</h2>
+        </Layout>
     )
 }
--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,32 @@
 import Head from 'next/head'
 import Layout, { siteTitle } from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
+import Link from 'next/link'
 
 import { getSortedPostsData } from '../lib/posts'
 
 export async function getStaticProps() {
-  const allPostsData = getSortedPostsData()
+  const allPostsData = getSortedPostsData();
+
+  const data = await fetch('https://randomuser.me/api/');
+  const randomuser = await data.json();
   return {
     props: {
-      allPostsData
+      allPostsData,
+      randomuser
     }
   }
 }
-export default function Home() {
+export default function Home({ allPostsData, randomuser }) {
+  const posts = allPostsData.map(({ id, date, title }) => (
+    <li className={utilStyles.listItem} key={id}>
+      {title}
+      <br />
+      {id}
+      <br />
+      {date}
+    </li>
+  ));
   return (
     <Layout home>
       <Head>
@@ -24,6 +38,12 @@ export default function Home() {
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
         </p>
+      </section>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {posts}
+        </ul>
       </section>
     </Layout>
   )
